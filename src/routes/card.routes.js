1717f6ceb4284db9dfd1f6d3d5f7b21c550e8318
shaziya78 <shaikhshaziya2828@gmail.com>
2025-03-22 15:33:58 +0530

    added giftcard by id

--- src/routes/card.routes.js
+++ src/routes/card.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCard ,updateCard , deleteCard, getAllGiftCards,getCategoriesGiftCards} from "../controllers/card.controller.js";
+import { createCard ,updateCard , deleteCard, getAllGiftCards,getCategoriesGiftCards,getGiftCardById} from "../controllers/card.controller.js";
 import { upload } from "../helper/multer.helper.js";
 export const cardRoute = Router();
 
@@ -7,5 +7,7 @@ cardRoute.route("/create-card").post(upload.single('img'),createCard);
 cardRoute.route("/update-card/:id").put(upload.single('img'),updateCard);
 cardRoute.route("/delete-card/:id").delete(deleteCard);
 cardRoute.route("/get-all-cards").get(getAllGiftCards);
-cardRoute.route("/get-cards-by-category/:category").get(getCategoriesGiftCards);
+cardRoute.route("/get-card/:id").get(getGiftCardById);
 
+cardRoute.route("/get-cards-by-category/:category").get(getCategoriesGiftCards);
+    
